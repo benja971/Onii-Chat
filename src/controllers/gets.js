ee2d2264975db4c3e9f593771dc54823c28cc86f
BENJAMIN NIDDAM <46868460+benja971@users.noreply.github.com>
2022-05-11 11:22:57 -0400

    get messages

--- src/controllers/gets.js
+++ src/controllers/gets.js
@@ -47,3 +47,44 @@ function getContacts(req, res) {
 
 	res.status(200).json(user.contacts);
 }
+
+/**
+ * @param {Request} req
+ * @param {Response} res
+ *
+ * req.query.conversation_id - conversation id
+ * req.query.from - start date
+ * req.query.to - end date
+ *
+ * @returns {message[]} - array of messages
+ *
+ * function that ask a conversation and (optionally) a date range and returns the messages
+ * in the conversation between the two dates or all the messages in the conversation if no date range is given
+ */
+function getMessages(req, res) {
+	const { conversation_id, from, to } = req.query;
+
+	// get the conversation from files
+	const conversation = loadJSON(`private/conversations/${conversation_id}.json`);
+
+	// check if conversation exists
+	if (!conversation) {
+		res.status(404).send("Conversation not found");
+		return;
+	}
+
+	// if asking for messages in a specific period of time
+	if (from && to) {
+		// get the messages from the conversation
+		const messages = conversation.messages.filter(message => message.date >= from && message.date <= to);
+
+		res.status(200).json(messages);
+	}
+
+	// if asking for all messages
+	else {
+		res.status(200).json(conversation.messages);
+	}
+}
+
+module.exports = { getUser, getContacts, getMessages };
--- src/route.js
+++ src/route.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
 const { test, sendMail } = require("./controllers/register");
+const login = require("./controllers/login");
+const { getUser, getContacts, getMessages } = require("./controllers/gets");
 
 // Route function called by both http and https servers
 module.exports = function route(app) {
@@ -13,8 +15,13 @@ module.exports = function route(app) {
 	app.post("/api/sendEmail", sendMail);
 
 	app.post("/api/login", login);
+
 	app.get("/api/user", getUser);
+
 	app.get("/api/getContacts", getContacts);
+
+	app.get("/api/getMessages", getMessages);
+
 	// 404
 	app.get("*", (req, res) => {
 		res.status(404).sendFile("404.html", { root: __dirname });
